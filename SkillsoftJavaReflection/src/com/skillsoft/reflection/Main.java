15128f832710d9d727a86c04a9951576dbd72279
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-02 18:00:33 -0600

    Getting and Setting Field Values

--- SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
@@ -1,112 +1,85 @@
 package com.skillsoft.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException,
+            InstantiationException, NoSuchFieldException {
 
         Class<?> employeeClass = Class.forName("com.skillsoft.reflection.Employee");
 
-        System.out.println("************* public fields");
+        Employee employee = (Employee) employeeClass.getConstructor().newInstance();
 
-        Field[] fields = employeeClass.getFields(); // gets public fields
+        System.out.println(employee); // 167349396, Name: Unknown, Title: Unknown, Salary: 0.0, Dept: null, Committees: null, Type: FULLTIME
 
-        for (Field field : fields) {
-            System.out.println(field);
-            // public java.lang.String com.skillsoft.reflection.Employee.name
-            // public java.lang.String com.skillsoft.reflection.Employee.title
-            // public volatile java.util.List com.skillsoft.reflection.Employee.committees
-        }
+        System.out.println("************** setting name and title");
 
-        System.out.println();
+        Field nameField = employeeClass.getField("name");
+        nameField.set(employee, "Jason");
 
-        System.out.println("************* public + protected + private fields");
+        Field titleField = employeeClass.getField("title");
+        titleField.set(employee, "Analyst");
 
-        fields = employeeClass.getDeclaredFields(); // gets public + protected + private fields
+        System.out.println(employee); //  167349396, Name: Jason, Title: Analyst, Salary: 0.0, Dept: null, Committees: null, Type: FULLTIME
 
-        for (Field field: fields) {
-            System.out.println(field);
-            // private static final transient java.util.Random com.skillsoft.reflection.Employee.employeeIdGenerator
-            // private final int com.skillsoft.reflection.Employee.employeeId
-            // public java.lang.String com.skillsoft.reflection.Employee.name
-            // public java.lang.String com.skillsoft.reflection.Employee.title
-            // private double com.skillsoft.reflection.Employee.salary
-            // private com.skillsoft.reflection.Department com.skillsoft.reflection.Employee.department
-            // public volatile java.util.List com.skillsoft.reflection.Employee.committees
-            // protected com.skillsoft.reflection.Employee$Type com.skillsoft.reflection.Employee.employeeType
-        }
+        System.out.println("Name: " + nameField.get(employee)); // Name: Jason
+        System.out.println("Title: " + titleField.get(employee)); // Title: Analyst
 
         System.out.println();
 
-        System.out.println("***************** field names, types, generic types");
-
-        for (Field field : fields) {
-            System.out.println("-----");
-            System.out.println("Name: " + field.getName()); // get name of the fields
-            System.out.println("Type: " + field.getType()); // get type of the fields
-            System.out.println("Generic Type: " + field.getGenericType()); // get type parameter of the fields
-            // -----
-            //Name: employeeIdGenerator
-            //Type: class java.util.Random
-            //Generic Type: class java.util.Random
-            //-----
-            //Name: employeeId
-            //Type: int
-            //Generic Type: int
-            //-----
-            //Name: name
-            //Type: class java.lang.String
-            //Generic Type: class java.lang.String
-            //-----
-            //Name: title
-            //Type: class java.lang.String
-            //Generic Type: class java.lang.String
-            //-----
-            //Name: salary
-            //Type: double
-            //Generic Type: double
-            //-----
-            //Name: department
-            //Type: class com.skillsoft.reflection.Department
-            //Generic Type: class com.skillsoft.reflection.Department
-            //-----
-            //Name: committees
-            //Type: interface java.util.List
-            //Generic Type: java.util.List<java.lang.String>
-            //-----
-            //Name: employeeType
-            //Type: class com.skillsoft.reflection.Employee$Type
-            //Generic Type: class com.skillsoft.reflection.Employee$Type
-        }
-
-        System.out.println();
-
-        System.out.println("*************** accessing fields by name");
-
-        Field nameField = employeeClass.getField("name"); // get public name field
-        Field titleField = employeeClass.getField("title"); // get public title field
-        Field committessField = employeeClass.getField("committees"); // get committees field
-
-        System.out.println(nameField); // public java.lang.String com.skillsoft.reflection.Employee.name
-        System.out.println(titleField); // public java.lang.String com.skillsoft.reflection.Employee.title
-        System.out.println(committessField); // public volatile java.util.List com.skillsoft.reflection.Employee.committees
-
-        System.out.println();
-
-//        System.out.println("************* accessing private fields by name (error)");
+//        Class<?> employeeClass = Class.forName("com.skillsoft.reflection.Employee");
 //
-//        Field salaryField = employeeClass.getField("salary"); // Field 'salary' is not public, java.lang.NoSuchFieldException
-//        Field employeeIdField = employeeClass.getField("employeeId"); // Field 'employeeId' is not public, java.lang.NoSuchFieldException
-
-        System.out.println("*************** accessing private fields by name");
-
-        Field salaryField = employeeClass.getDeclaredField("salary"); // get private salary field
-        Field employeeIdField = employeeClass.getDeclaredField("employeeId"); // get private employeeId field
-
-        System.out.println(salaryField); // private double com.skillsoft.reflection.Employee.salary
-        System.out.println(employeeIdField); // private final int com.skillsoft.reflection.Employee.employeeId
-
-        System.out.println();
+//        System.out.println("*********** field modifiers");
+//
+//        Field[] fields = employeeClass.getDeclaredFields();
+//
+//        for (Field field : fields) {
+//
+//            int modifier = field.getModifiers();
+//            System.out.println("---------------");
+//            System.out.println(field.getName());
+//            System.out.println(Integer.toBinaryString(modifier));
+//            System.out.println(Modifier.toString(modifier)); // get all the modifiers as String representation for the int modifier value
+//            // ---------------
+//            //employeeIdGenerator
+//            //10011010
+//            //private static final transient
+//            //---------------
+//            //employeeId
+//            //10010
+//            //private final
+//            //---------------
+//            //name
+//            //1
+//            //public
+//            //---------------
+//            //title
+//            //1
+//            //public
+//            //---------------
+//            //salary
+//            //10
+//            //private
+//            //---------------
+//            //department
+//            //10
+//            //private
+//            //---------------
+//            //committees
+//            //1000001
+//            //public volatile
+//            //---------------
+//            //employeeType
+//            //100
+//            //protected
+//        }
+//
+//        System.out.println();
     }
 }
 
