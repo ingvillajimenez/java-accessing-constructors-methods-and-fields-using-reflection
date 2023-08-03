2d4315c949e885588c3a53ad98ad9fe255a32d43
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-03 07:37:15 -0600

    Accessing Annotations on Methods

--- SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
@@ -1,123 +1,49 @@
 package com.skillsoft.reflection;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
+
+    private static void printMethodProperties(Method method) {
+
+        System.out.println("-----------------");
+        System.out.println(method.getName());
+        System.out.println("Parameter count: " + method.getParameterCount());
+        System.out.println("Parameter types: " + Arrays.toString(method.getParameterTypes()));
+        System.out.println("Return type: " + method.getReturnType());
+
+        System.out.println("Exception types: " + Arrays.toString(method.getExceptionTypes()));
+
+        // NOTE: Returns annotations that are directly present on this element.
+        // NOTE: The @Override and @SuppressWarnings annotation is not retained for
+        // reflective access
+        System.out.println("Annotations: " + Arrays.toString(method.getDeclaredAnnotations()));
+        System.out.println("Check for @Override annotation: " +
+                method.getAnnotation(Override.class)); // Annotation 'Override.class' is not retained for reflective access, retention policy is set to source
+        System.out.println("Check for @SuppressWarnings annotation: " +
+                method.getAnnotation(SuppressWarnings.class)); // Annotation 'SuppressWarning.class' is not retained for reflective access
+        System.out.println("Check for @Deprecated annotation: " +
+                method.getAnnotation(Deprecated.class)); // retention policy is set to runtime
+
+        int modifiers = method.getModifiers();
+
+        System.out.println("Modifiers: " + Integer.toBinaryString(modifiers));
+        System.out.println("Modifiers (string representation): " + Modifier.toString(modifiers));
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
 
         Class<?> employeeClass = Class.forName("com.skillsoft.reflection.Employee");
 
-        System.out.println("************** accessing method properties");
+        System.out.println("*************** accessing method by name");
 
-        Method[] methods = employeeClass.getDeclaredMethods(); // gets public, protected, private methods
+        Method toStringMethod = employeeClass.getMethod("toString");
+        printMethodProperties(toStringMethod);
 
-        for (Method method: methods) {
-            System.out.println("-------------");
-            System.out.println(method.getName());
-            System.out.println("Parameter count: " + method.getParameterCount());
-            System.out.println("Parameter types: " + Arrays.toString(method.getParameterTypes()));
-            System.out.println("Return type: " + method.getReturnType());
-            System.out.println("Exception types: " + Arrays.toString(method.getExceptionTypes()));
-            // -------------
-            //getName
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: class java.lang.String
-            //Exception types: []
-            //-------------
-            //toString
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: class java.lang.String
-            //Exception types: []
-            //-------------
-            //setName
-            //Parameter count: 1
-            //Parameter types: [class java.lang.String]
-            //Return type: void
-            //Exception types: []
-            //-------------
-            //save
-            //Parameter count: 3
-            //Parameter types: [class java.lang.String, class java.lang.String, class java.lang.String]
-            //Return type: boolean
-            //Exception types: [class java.lang.IllegalAccessException, class java.lang.InterruptedException, class java.util.concurrent.ExecutionException]
-            //-------------
-            //save
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: boolean
-            //Exception types: [class java.lang.IllegalAccessException]
-            //-------------
-            //save
-            //Parameter count: 1
-            //Parameter types: [class java.lang.String]
-            //Return type: boolean
-            //Exception types: [class java.lang.IllegalAccessException, class java.lang.InterruptedException]
-            //-------------
-            //computeBonusInternal
-            //Parameter count: 1
-            //Parameter types: [float]
-            //Return type: double
-            //Exception types: []
-            //-------------
-            //createEmployee
-            //Parameter count: 3
-            //Parameter types: [class java.lang.String, class java.lang.String, double]
-            //Return type: class com.skillsoft.reflection.Employee
-            //Exception types: []
-            //-------------
-            //getEmployeeId
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: int
-            //Exception types: []
-            //-------------
-            //computeTotalSalary
-            //Parameter count: 1
-            //Parameter types: [float]
-            //Return type: double
-            //Exception types: []
-            //-------------
-            //incrementSalary
-            //Parameter count: 1
-            //Parameter types: [float]
-            //Return type: void
-            //Exception types: []
-            //-------------
-            //printEmployeeDetails
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: void
-            //Exception types: []
-            //-------------
-            //getTitle
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: class java.lang.String
-            //Exception types: []
-            //-------------
-            //setTitle
-            //Parameter count: 1
-            //Parameter types: [class java.lang.String]
-            //Return type: void
-            //Exception types: []
-            //-------------
-            //getSalary
-            //Parameter count: 0
-            //Parameter types: []
-            //Return type: double
-            //Exception types: []
-            //-------------
-            //setSalary
-            //Parameter count: 1
-            //Parameter types: [double]
-            //Return type: void
-            //Exception types: []
-        }
-
-        System.out.println();
     }
 }
 
